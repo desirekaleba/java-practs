fb0ebf4994448213c795529a63ae6b1ebbe8cc1d
Desire Kaleba <desirekaleba@gmail.com>
2019-03-17 03:27:38 +0300

    add test proxy

---
+++ TestProxy/src/SublimeEssaie.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+public class SublimeEssaie
+{
+	public static void main(String[] args){
+            int d = (int)Math.pow(2, 4);
+            System.out.println(d);
+        }
+}
