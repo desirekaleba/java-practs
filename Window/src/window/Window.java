7e9d441706e373ad1ed314850f84d4aa7a0cd667
Desire Kaleba <desirekaleba@gmail.com>
2019-03-24 03:28:22 +0300

    Add window

---
+++ Window/src/window/Window.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package window;
+
+/**
+ *
+ * @author desire
+ */
+public class Window {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        javax.swing.JFrame frame = new javax.swing.JFrame();
+        frame.setTitle("Title");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+    }
+    
+}
